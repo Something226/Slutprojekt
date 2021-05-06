499d395f62805807c8c5acffe194de922dc6fbb3
Something226 <axel.lieback@gmail.com>
2021-05-06 22:16:55 +0200

    Intial Commit

---
+++ Slutprojekt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Slutprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Intialtext();
+
+            Console.ReadLine();
+
+        }
+
+        static void Intialtext(){
+
+            System.Console.WriteLine("Hi, welcome to the shop!\nWhat Do you want to buy?");
+
+        }
+
+        static void Itemlist(){
+            
+        }
+    }   
+}
